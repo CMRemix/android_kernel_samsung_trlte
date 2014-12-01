301f65b1c290577fa90ce71c6803dfe9a1286fe5
imoseyon <imoseyon@gmail.com>
2014-11-30 19:15:28 -0800

    cpufreq: interactiveX: create a matching sysfs group for interactiveX
    Some apps (like Kernel Tweaker) follows the governor name for sysfs
    nodes. I could not figure out a way to symlink it to interactive (todo
    later).
    
    (cherry picked from commit 277cb9cdf755eeaa8ec616e4b2041d718d1e0b09)
    
    Change-Id: Iec1378b8a612c81c271f0c5795629377e434d224
    
    (cherry picked from commit 6f64e85e5f7bb1faa22d5ff420c673457714a755)
    
    (cherry picked from commit 598f5a17840154be8d6d7234e378948907dae4e5)

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -1690,6 +1690,11 @@ static struct attribute_group interactive_attr_group = {
 	.name = "interactive",
 };
 
+static struct attribute_group interactivex_attr_group = {
+	.attrs = interactive_attributes,
+	.name = "interactiveX",
+};
+
 static int cpufreq_interactive_idle_notifier(struct notifier_block *nb,
 					     unsigned long val,
 					     void *data)
@@ -1793,6 +1798,9 @@ static int cpufreq_governor_interactive(struct cpufreq_policy *policy,
 			return rc;
 		}
 
+		rc = sysfs_create_group(get_governor_parent_kobj(policy),
+				&interactivex_attr_group);
+
 		idle_notifier_register(&cpufreq_interactive_idle_nb);
 		cpufreq_register_notifier(
 			&cpufreq_notifier_block, CPUFREQ_TRANSITION_NOTIFIER);
@@ -1821,6 +1829,8 @@ static int cpufreq_governor_interactive(struct cpufreq_policy *policy,
 		idle_notifier_unregister(&cpufreq_interactive_idle_nb);
 		sysfs_remove_group(get_governor_parent_kobj(policy),
 				&interactive_attr_group);
+		sysfs_remove_group(get_governor_parent_kobj(policy),
+				&interactivex_attr_group);
 		if (!have_governor_per_policy())
 			cpufreq_put_global_kobject();
 		mutex_unlock(&gov_lock);
